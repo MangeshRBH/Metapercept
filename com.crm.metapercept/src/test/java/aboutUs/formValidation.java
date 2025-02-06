80f119d223424c5c0b46415fab307340655ea6e1
MangeshRBH <mangesh.rb@metapercept.com>
2025-02-06 17:13:09 +0530

    init

--- com.crm.metapercept/src/test/java/aboutUs/formValidation.java
+++ com.crm.metapercept/src/test/java/aboutUs/formValidation.java
@@ -1,5 +1,27 @@
 package aboutUs;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class formValidation {
 
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://metapercept.com/");
+		Thread.sleep(5000);
+		driver.findElement(By.xpath("//button[@id='rcc-confirm-button']")).click();
+		Thread.sleep(5000);
+		WebElement chatFrame =  driver.findElement(By.xpath("//*[@id='siqiframe']"));
+		driver.switchTo().frame(chatFrame);
+		driver.findElement(By.xpath("//*[@id='min_window']")).click();
+		driver.switchTo().parentFrame();
+		
+	}
+	
 }
